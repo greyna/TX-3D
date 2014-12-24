820dd6203b1d828fd670523d8501fcd68e985688
Arthur Van Ceulen <arthur.van-ceulen@greyna.eu>
2014-12-24 18:10:22 +0100

    light debugged

--- TX-3D/Light.cpp
+++ TX-3D/Light.cpp
@@ -5,7 +5,7 @@
 Light::Light():
 	pos(0.0, 0.0, 2.0), spec(1.0, 1.0, 1.0), dif(0.7, 0.7, 0.7), amb(0.2, 0.2, 0.2)
 {
-	this->position = Uniform::createUniformVec3fv("position", pos.v);
+	this->position = Uniform::createUniformVec3fv("light_position", pos.v);
 	this->specular = Uniform::createUniformVec3fv("Ls", spec.v);
 	this->diffuse = Uniform::createUniformVec3fv("Ld", dif.v);
 	this->ambient = Uniform::createUniformVec3fv("La", amb.v);
