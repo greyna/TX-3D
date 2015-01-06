e4a9375fd8e617318a5f67e1beeb8c1d2243a23e
Arthur Van Ceulen <arthur.van-ceulen@greyna.eu>
2015-01-06 11:17:54 +0100

    oculus sensors recuperation

--- TX-3D/Oculus.cpp
+++ TX-3D/Oculus.cpp
@@ -1,8 +1,10 @@
 #include "Oculus.h"
+#include "OVR_Kernel.h"
 #include <iostream>
 
-Oculus::Oculus():
-hmd()
+using namespace OVR;
+
+Oculus::Oculus() : hmd(), posTracked(false), oriTracked(false), pose()
 {
 	ovr_Initialize();
 
@@ -11,11 +13,19 @@ hmd()
 	{
 		// Get more details about the HMD.
 		ovrSizei resolution = hmd->Resolution;
+		// Extract tracking frustum parameters. (external camera)
+		float frustomHorizontalFOV = hmd->CameraFrustumHFovInRadians;
+
+		// Start the sensor which provides the Rift’s pose and motion.
+		ovrHmd_ConfigureTracking(hmd, ovrTrackingCap_Orientation |
+			ovrTrackingCap_MagYawCorrection |
+			ovrTrackingCap_Position, 0);
+
+		
 	}
 	else std::cerr << "hmd not detected" << std::endl;
 
 }
-
 
 Oculus::~Oculus()
 {
@@ -24,4 +34,40 @@ Oculus::~Oculus()
 		ovrHmd_Destroy(hmd);
 	}
 	ovr_Shutdown();
+}
+
+void Oculus::querySensors()
+{
+	// Query the HMD for the current tracking state.
+	ovrTrackingState ts = ovrHmd_GetTrackingState(hmd, getTimeSec());
+	posTracked = ts.StatusFlags & ovrStatus_PositionTracked;
+	oriTracked = ts.StatusFlags & ovrStatus_OrientationTracked;
+
+	//if (ts.StatusFlags & (ovrStatus_OrientationTracked | ovrStatus_PositionTracked))
+	if (posTracked || oriTracked)
+	{
+		pose = ts.HeadPose.ThePose;
+		// example
+		float yaw, eyePitch, eyeRoll;
+		pose.Rotation.GetEulerAngles<Axis_Y, Axis_X, Axis_Z>(&yaw, &eyePitch, &eyeRoll);
+	}
+}
+
+bool Oculus::isSupported()
+{
+	return hmd;
+}
+
+void Oculus::recenter() {
+	ovrHmd_RecenterPose(hmd);
+}
+
+double Oculus::getTimeSec()
+{
+	return ovr_GetTimeInSeconds();
+}
+
+void Oculus::dismissWarning()
+{
+	ovrHmd_DismissHSWDisplay(hmd);
 }
--- TX-3D/Oculus.h
+++ TX-3D/Oculus.h
@@ -3,12 +3,21 @@
 #include "OVR.h"
 #include "OVR_Version.h"
 
+using namespace OVR;
+
 class Oculus
 {
 private:
 	ovrHmd hmd;
+	bool posTracked, oriTracked;
+	Posef pose;
+	void querySensors();
 public:
 	Oculus();
 	~Oculus();
+	bool isSupported();
+	void recenter();
+	double getTimeSec();
+	void dismissWarning();
 };
 
